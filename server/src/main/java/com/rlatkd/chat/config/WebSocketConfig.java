3cb1f9af0e97352bbfaa6631bf0e624eb07f0a8c
Sanghun Kim <rlatkdgns042@naver.com>
2024-06-02 01:42:21 +0900

    Update WebSocketConfig.java

--- server/src/main/java/com/rlatkd/chat/config/WebSocketConfig.java
+++ server/src/main/java/com/rlatkd/chat/config/WebSocketConfig.java
@@ -28,4 +28,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/kafka");
         registry.enableSimpleBroker("/topic/");
     }
+    
 }
